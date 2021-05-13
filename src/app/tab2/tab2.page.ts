ed8938aeded55d84f1187a6020b6342fc84390b4
alnbastos <alinebastos23@outlook.com>
2021-05-13 14:50:10 -0300

    pagina calculadora imc

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  peso: number;
+  altura: number;
+  imc: number;
 
-  constructor() {}
+  constructor(){
 
+  }
+
+  calcularImc(): number{
+    let imc = 0;
+    let alt = this.altura;
+    let pes = this.peso;
+
+    this.imc = pes / (alt ** 2)
+    alert(this.imc);
+
+    return this.imc;
+  }
 }
